22368823543931e08150223df476c27cdd3fe103
Julio <julianoandradeo10@gmail.com>
2024-04-04 21:51:57 -0300

    Testes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Sobre from './componentes/Sobre'
 
 function App() {
   return (
     <div className="App">
       <h1>Meu projeto ReactJs</h1>
+      <Sobre />
     </div>
   );
 }
