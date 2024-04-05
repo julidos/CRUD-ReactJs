0e115f7f70c34b09147028035872f366eb71268c
Julio <julianoandradeo10@gmail.com>
2024-04-04 22:01:32 -0300

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Sobre from './componentes/Sobre'
+import Home from './componentes/Home'
+import Alunos from './componentes/Alunos'
 
 function App() {
   return (
     <div className="App">
       <h1>Meu projeto ReactJs</h1>
-      <Sobre />
+      <ul>
+        <li>Página Inicial</li>
+        <li>Cadastro de Alunos</li>
+        <li>Sobre</li>
+      </ul>
     </div>
   );
 }
