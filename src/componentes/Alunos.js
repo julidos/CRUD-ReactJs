16e144a171fd925b72713f36b6c7e4d73d44f504
Julio <julianoandradeo10@gmail.com>
2024-04-13 12:00:54 -0300

    Del API / Add cadastro Aluno

--- src/componentes/Alunos.js
+++ src/componentes/Alunos.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table, Button, Forms } from "react-bootstrap";
+import { Table, Button, Form } from "react-bootstrap";
 
 class Alunos extends React.Component {
 
@@ -7,6 +7,9 @@ class Alunos extends React.Component {
         super(props);
 
         this.state = {
+            nome: '',
+            email: '',
+            curso: '',
             alunos: []
         }
     }
@@ -35,6 +38,21 @@ class Alunos extends React.Component {
                 }
                 else {
                     alert("Aluno não encontrado")
+                }
+            })
+    }
+
+    cadastraAluno(aluno) {
+        fetch("http://localhost:5001/alunos/",
+            { method: 'POST' ,
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(aluno)
+        })
+            .then(resposta => {
+                if (resposta.ok) {
+                    this.buscarAluno();
+                } else{
+                    alert('Não foi possivel adicionar o aluno !')
                 }
             })
     }
@@ -71,9 +89,62 @@ class Alunos extends React.Component {
             </Table>
         )
     }
+
+    atualizaNome = (e) => {
+        this.setState(
+            {
+                nome: e.target.value
+            }
+        )
+    }
+
+    atualizaEmail = (e) => {
+        this.setState(
+            {
+                email: e.target.value
+            }
+        )
+    }
+
+    atualizaCurso = (e) => {
+        this.setState(
+            {
+                curso: e.target.value
+            }
+        )
+    }
+
+    submit() {
+        const aluno = {
+            id: 0,
+            nome: this.state.nome,
+            email: this.state.email,
+            curso: this.state.curso
+        }
+
+        this.cadastraAluno(aluno)
+    }
+
     render() {
         return (
             <div>
+                <Form>
+                    <Form.Group className="mb-3" controlId="nomeAluno">
+                        <Form.Label>Nome</Form.Label>
+                        <Form.Control type="text" placeholder="Digite o nome do aluno" value={this.state.nome} onChange={this.atualizaNome} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="emailAluno">
+                        <Form.Label>E-mail</Form.Label>
+                        <Form.Control type="email" placeholder="Digite o email do aluno" value={this.state.email} onChange={this.atualizaEmail} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="cursoAluno">
+                        <Form.Label>Curso</Form.Label>
+                        <Form.Control type="text" placeholder="Digite o curso do aluno" value={this.state.curso} onChange={this.atualizaCurso} />
+                    </Form.Group>
+                    <Button className="mb-3" variant="primary" type="submit">
+                        Salvar
+                    </Button>
+                </Form>
                 {this.renderTabela()};
             </div>
         )
