6d6dc19979182b94c287ff6c0057aed17005d5f1
Julio <julianoandradeo10@gmail.com>
2024-04-13 19:20:39 -0300

    Del API para outro repositório / Add cadastro Aluno

--- src/componentes/Alunos.js
+++ src/componentes/Alunos.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table, Button, Forms } from "react-bootstrap";
+import { Table, Button, Form, FloatingLabel } from "react-bootstrap";
 
 class Alunos extends React.Component {
 
@@ -7,16 +7,17 @@ class Alunos extends React.Component {
         super(props);
 
         this.state = {
-            alunos: []
-        }
+            id: 0,
+            nome: '',
+            email: '',
+            curso: '',
+            alunos: [],
+        };
+        this.salvar = this.salvar.bind(this);
     }
 
     componentDidMount() {
         this.buscarAluno();
-    }
-
-    componentWillUnmount() {
-
     }
 
     buscarAluno = () => {
@@ -39,9 +40,43 @@ class Alunos extends React.Component {
             })
     }
 
+    cadastraAluno = (aluno) => {
+        fetch('http://localhost:5001/alunos/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(aluno)
+        }).then((resposta) => {
+
+            if (resposta.ok) {
+                this.buscarAluno();
+            } else {
+                alert("Não foi possivel adicionar o aluno");
+            }
+        }).catch(console.log);
+    }
+
+
+    atualizaNome = (e) => {
+        this.setState({
+            nome: e.target.value
+        });
+    }
+
+    atualizaEmail = (e) => {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    atualizaCurso = (e) => {
+        this.setState({
+            curso: e.target.value
+        });
+    }
+
     renderTabela() {
         return (
-            <Table striped bordered hover variant="dark">
+            <Table striped bordered hover className="ms-2">
                 <thead>
                     <tr>
                         <th>id</th>
@@ -55,14 +90,14 @@ class Alunos extends React.Component {
 
                     {
                         this.state.alunos.map((aluno) =>
-                            <tr>
+                            <tr key={aluno.id}>
                                 <td>{aluno.id}</td>
                                 <td>{aluno.nome}</td>
                                 <td>{aluno.email}</td>
                                 <td>{aluno.curso}</td>
                                 <td>
-                                    <Button variant="success">Alterar</Button>
-                                    <Button variant="danger" onClick={() => this.deletarAluno(aluno.id)}>Excluir</Button>
+                                    <Button variant="success">Atualizar</Button>
+                                    <Button className="ms-1" variant="danger" onClick={() => this.deletarAluno(aluno.id)}>Excluir</Button>
                                 </td>
                             </tr>
                         )
@@ -71,9 +106,56 @@ class Alunos extends React.Component {
             </Table>
         )
     }
+
+    salvar = () => {
+        const aluno = {
+            id: 0,
+            nome: this.state.nome,
+            email: this.state.email,
+            curso: this.state.curso
+
+        }
+
+        this.cadastraAluno(aluno);
+
+        this.setState({
+            nome: '',
+            email: '',
+            curso: ''
+        });
+    }
+
     render() {
         return (
             <div>
+                <Form>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Nome"
+                        className="mb-2 ms-2 "
+                    >
+                        <Form.Control type="text" placeholder="Nome Sobrenome" value={this.state.nome} onChange={this.atualizaNome} />
+                    </FloatingLabel>
+
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Email"
+                        className="mb-2 ms-2"
+                    >
+                        <Form.Control type="email" placeholder="nome@exemplo.com" value={this.state.email} onChange={this.atualizaEmail} />
+                    </FloatingLabel>
+
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Curso"
+                        className="mb-2 ms-2"
+                    >
+                        <Form.Control type="text" placeholder="Curso" value={this.state.curso} onChange={this.atualizaCurso} />
+                    </FloatingLabel>
+                    <Button className="mb-2 ms-2" variant="primary" onClick={this.salvar} >
+                        Salvar
+                    </Button>
+                </Form>
                 {this.renderTabela()};
             </div>
         )
